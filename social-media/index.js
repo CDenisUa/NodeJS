d316b480d92e36584490427362fce82abb1e8a9e
Denys Chepiha <dchepiha@exoft.net>
2022-03-26 18:47:23 +0200

    social-media finish develop API

---
+++ social-media/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, () => console.log('DB connected'));
+
+const app = express();
+app.listen(8800,() => console.log('Backend server is running...'));
+app.use(express.json());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
