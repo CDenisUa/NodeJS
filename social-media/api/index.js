ce8f20aa4c278b652cad5767a83f0351feee6502
Denys Chepiha <chepigadeveloper@gmail.com>
2022-05-08 02:06:45 +0200

    Add action create post

--- social-media/api/index.js
+++ social-media/api/index.js
@@ -3,20 +3,41 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const cors = require('cors');
+const path = require("path");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-const cors = require('cors')
 
 dotenv.config();
 
+const app = express();
 mongoose.connect(process.env.MONGO_LOCAL_URL, () => console.log('DB connected'));
 
-const app = express();
 app.listen(8800,() => console.log('Backend server is running...'));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(cors())
 app.use(express.json());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try{
+        return res.status(200).json("File uploaded successfully.")
+    }catch (error){
+        console.warn(error);
+    }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
--- social-media/front/src/components/share/Share.jsx
+++ social-media/front/src/components/share/Share.jsx
@@ -1,24 +1,73 @@
 import {EmojiEmotions, Label, PermMedia, Room} from '@material-ui/icons';
+import {useContext, useRef, useState} from "react";
+import {AuthContext} from "../../context/AuthContext";
 import './share.css';
+import axios from "axios";
 
 export default function Share() {
+    const { user } = useContext(AuthContext);
+    const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
+    const descriptionRef = useRef();
+    const [ file, setFile ] = useState(null);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            userId: user._id,
+            description: descriptionRef.current.value,
+        };
+        if (file) {
+            const data = new FormData();
+            const fileName = Date.now() + file.name;
+            data.append("name", fileName);
+            data.append("file", file);
+            newPost.img = fileName;
+            console.log(newPost);
+            try {
+                await axios.post("/upload", data);
+            } catch (err) {}
+        }
+        try {
+            await axios.post("/posts", newPost);
+            // window.location.reload();
+        } catch (err) {}
+    };
+
     return (
         <div className='share'>
             <div className="share-wrapper">
                 <div className="share-top">
-                    <img className='share-profile-img ' src={"/assets/person/1.jpeg"} alt="" />
+                    <img className='share-profile-img ' src={ user.profilePicture ? user.profilePicture : `${publicFolder}person/noAvatar.jpeg` } alt="" />
                     <input
-                        placeholder="What's is your mind Denis?"
+                        ref={descriptionRef}
+                        placeholder={`What's is your mind ${user.username}?`}
                         type="text"
-                        className='share-input'/>
+                        className='share-input'
+                    />
                 </div>
                 <hr className='share-hr' />
-                <div className="share-bottom">
+                <form
+                    onSubmit={submitHandler}
+                    className="share-bottom"
+                >
                     <div className="share-options">
-                        <div className="share-option">
-                            <PermMedia htmlColor="tomato" className='share-icon' />
+                        <label
+                            htmlFor="file"
+                            className="share-option"
+                        >
+                            <PermMedia
+                                htmlColor="tomato"
+                                className='share-icon'
+                            />
                             <span className="share-option-text">Photo or Video</span>
-                        </div>
+                            <input
+                                style={{display: "none"}}
+                                id="file"
+                                type="file"
+                                accept=".png,.jpeg,.jpg"
+                                onChange={(event) => setFile(event.target.files[0])}
+                            />
+                        </label>
                         <div className="share-option">
                             <Label htmlColor="#9CCEFF" className='share-icon' />
                             <span className="share-option-text">Tag</span>
@@ -32,8 +81,13 @@ export default function Share() {
                             <span className="share-option-text">Feelings</span>
                         </div>
                     </div>
-                    <button className="share-button">Share</button>
-                </div>
+                    <button
+                        type="submit"
+                        className="share-button"
+                    >
+                        Share
+                    </button>
+                </form>
             </div>
         </div>
     )
