4beed741619bbd98cf57000a847aab3210e4051f
Denys <chepigadeveloper@gmail.com>
2024-08-15 21:55:29 +0200

    Added c-commonjs project to  study

---
+++ c-commonjs/db/setup.sh
@@ -0,0 +1,5 @@
+psql -f install.sql -U postgres
+PGPASSWORD=admin psql -h localhost -p 5432 -U postgres -d postgres -f install.sql
+PGPASSWORD=admin psql -h localhost -p 5432 -U marcus -d example -f structure.sql
+PGPASSWORD=admin psql -h localhost -p 5432 -U marcus -d example -f data.sql
+Изменения:cus
